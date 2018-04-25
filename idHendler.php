b108c3ab36f5f7ef99c0aab0fa44941dbeac8292
Ilja Kazantsev <33126420+4568520@users.noreply.github.com>
2018-04-25 17:11:54 +0300

    handler start

--- idHendler.php
+++ idHendler.php
@@ -24,7 +24,30 @@ function idCheck($id){
         echo 'Wrong gender';
         return 0;
     }
-    $curDate=
+    $curDate=getdate();
+    $cY=intval(substr($curDate['year'],2,2));
+    $cBY=intval($curDate['year']);
+    $cM=intval($curDate['mon']);
+    $cD=intval($curDate['mday']);
+    $yOB = intval(substr($id,1,2));
+    $mOB = intval(substr($id,4,2));
+    $dOB =intval(substr($id,6,2));
+    $days=[31,28,31,30,31,30,31,31,30,31,30,31];
+    if(($gender==6 or $gender==5)and yOB>cY){
+        echo 'year is bad';
+        return 0;
+    }
+    if($mOB>12 or $mOB<0){
+        echo 'Month is bad';
+        return 0;
+    }
+    if($gender==6 or $gender==5){
+        $yOB+=2000;
+    }elseif ($gender==4 or $gender==3){
+        $yOB+=1900;
+    }else{
+        $yOB+=1800;
+    }
 
 }
 
