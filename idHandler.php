a175a11d23a64fdb933b4b6b8ded90f9a7e5f684
Ilja Kazantsev <ilja.kazantsev@tptlive.ee>
2018-05-02 10:40:45 +0300

    Looks nicer

--- idHandler.php
+++ idHandler.php
@@ -4,65 +4,68 @@
  * Date: 04/24/18
  * Time: 19:25
  */
-function idCheck($id){
-    if(strlen($id)==0){//checking if id is a valid string
-    echo 'no data';
-    return -1;
+function idCheck($id) 
+{
+    if (strlen($id)==0) {//checking if id is a valid string
+        echo 'no data';
+        return -1;
 
-    }elseif (strlen($id)!=11){
+    } elseif (strlen($id)!=11) {
 
         echo 'wrong size of string';
         return -1;
-    }else{
-        if(!is_numeric($id)){
+    } else {
+        if (!is_numeric($id)) {
             echo 'Not a number';
             return 0;
         }
 
-    $gender=intval($id[0]);
-    if($gender>6 or $gender<1){
-        echo 'Wrong gender';
-        return -1;
+        $gender=intval($id[0]);
+        if ($gender>6 or $gender<1) {
+            echo 'Wrong gender';
+             return -1;
+        }
+        $curDate=getdate();
+        $cBY=intval($curDate['year']);
+        $cY=intval(substr($curDate['year'], 2, 2));
+        $cM=intval($curDate['mon']);
+        $cD=intval($curDate['mday']);
+        $yOB = intval(substr($id, 1, 2));
+        $mOB = intval(substr($id, 3, 2));
+        $dOB =intval(substr($id, 5, 2));
+        $days=[31,28,31,30,31,30,31,31,30,31,30,31];
+        if (($gender==6 or $gender==5)and $yOB>$cY) {
+            echo 'year is bad<br>';
+            return -1;
+        }
+        if ($mOB>12 or $mOB<1) {
+            echo 'Month is bad<br>';
+            return -1;
+        }
+        if ($gender==6 or $gender==5) {
+            $yOB+=2000;
+        } elseif ($gender==4 or $gender==3) {
+            $yOB+=1900;
+        } else {
+            $yOB+=1800;
+        }
+        if ($yOB%4==0) {
+            $days[1]=29;
+        }
+        $mOB--;
+        if (($dOB>$days[$mOB]) or $dOB==0) {
+            echo 'Day is bad<br>';
+            return -1;
+        }
+        $mOB++;
+        $age=$cBY-$yOB-1;
+        if ($mOB<$cM) {
+            $age++;
+        } elseif ($mOB==$cM and $dOB<=$cD) {
+            $age++;
+        }
+        return $age;
     }
-    $curDate=getdate();
-    $cY=intval(substr($curDate['year'],2,2));
-    $cBY=intval($curDate['year']);
-    $cM=intval($curDate['mon']);
-    $cD=intval($curDate['mday']);
-    $yOB = intval(substr($id,1,2));
-    $mOB = intval(substr($id,3,2));
-    $dOB =intval(substr($id,5,2));
-    $days=[31,28,31,30,31,30,31,31,30,31,30,31];
-    if(($gender==6 or $gender==5)and $yOB>$cY){
-        echo 'year is bad<br>';
-        return -1;
-    }
-    if($mOB>12 or $mOB<1){
-        echo 'Month is bad<br>';
-        return -1;
-    }
-    if($gender==6 or $gender==5){
-        $yOB+=2000;
-    }elseif ($gender==4 or $gender==3){
-        $yOB+=1900;
-    }else{
-        $yOB+=1800;
-    }
-    if($yOB%4==0){
-        $days[1]=29;
-    }
-    $mOB--;
-    if(($dOB>$days[$mOB]) or $dOB==0){
-        echo 'Day is bad<br>';
-        return -1;
-    }
-    $mOB++;
-    $age=$cBY-$yOB-1;
-    if($mOB<$cM){
-        $age++;
-    }elseif ($mOB==$cM and $dOB<=$cD){
-     $age++;
-    }
-    return $age;
-}}
+}
 ?>
+
--- index.php
+++ index.php
@@ -1,16 +1,11 @@
 <?php
-/**
- * Created by PhpStorm.
- * Date: 04/21/18
- * Time: 13:47
- */
-include 'idHandler.php';
+require 'idHandler.php';
 $host='localhost';
 $user='root';
 $psswd='';
 $db='names';
 
-$link=mysqli_connect($host,$user,$psswd);
+$link=mysqli_connect($host, $user, $psswd);
 
 if ($link->connect_error) {
     die("Connection failed: <br>" . $link->connect_error);
@@ -25,15 +20,15 @@ mysqli_query($link, "COLLATE='utf8_general_ci';");
 mysqli_query($link, 'SET NAMES utf8;');
 mysqli_select_db($link, $db);
 
-mysqli_query($link,'CREATE TABLE IF NOT EXISTS `people` (
+mysqli_query(
+    $link, 'CREATE TABLE IF NOT EXISTS `people` (
 	`id` CHAR(13) NOT NULL,
 	`name` CHAR(13) NOT NULL,
 	`lastname` CHAR(13) NOT NULL,
 	`age` SMALLINT NOT NULL,
 	PRIMARY KEY (`id`)
-)
-COLLATE=\'utf8_general_ci\'
-ENGINE=InnoDB');
+)COLLATE=\'utf8_general_ci\'ENGINE=InnoDB'
+);
 
 //`age` int(3) NOT NULL,
 //,"'.$age.'"
@@ -42,29 +37,31 @@ $fileN = fopen("name.txt","r");
 $index=0;
 $idArray=[0];
 
-do{
+do {
 
     $peopleSTR=fgets($fileN);
     $people=explode(" ",$peopleSTR);
-    if(!array_search($people[2],$idArray)) {
+    if (!array_search($people[2], $idArray)) {
         //exec('py C:\laragon\www\dbNamesPHP\ikDEcoder.py '.$people[2],$age);
-        $age=idCheck(substr($people[2],0,11));
+        $age=idCheck(substr($people[2], 0, 11));
         if(mysqli_query($link, 'INSERT INTO people 
 VALUES("'.$people[2].'","'.$people[0].'","'.$people[1].'","'.$age.'"); ')=== TRUE) {
-    } else {
-        //echo "Can't add<br> ";
-    }
+        } else {
+            //echo "Can't add<br> ";
+        }
         $idArray[$index] = $people[2];
         $index++;
     }
 }while(!feof($fileN));
 fclose($fileN);
-if($test=mysqli_query($link,'SELECT name,lastname FROM people WHERE age>0 
+if ($test=mysqli_query($link, 'SELECT name,lastname FROM people WHERE age>0 
 AND age = (SELECT MIN(age) FROM people);')) {
     while ($rowOne = mysqli_fetch_assoc($test)) {
         echo "<li>" . $rowOne['name'] .' '.$rowOne['lastname'].'</li>';
     }
-}else {echo 'errorrrr';}
+} else {
+    echo 'errorrrr';
+}
 
 
 ?>
